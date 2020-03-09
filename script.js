5b40eb809d55adceb23a76dcb39e362b98100fb0
s2q1ne <amgkd9@naver.com>
2020-03-09 22:54:50 +0900

    / error fixed

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var convert = ()=>{
     let beforeText = new String(document.getElementById("before").value);
     beforeText=beforeText
     .replace(/([0-9]+)\s([a-z|A-Z]+)(\s[a-z]{1,2}\.)/gi,"$2;")
+    .replace(/\s\//gi,",")
     .replace(/\s[a-z]{1,2}\./gi,",")
     .replace(/\n([^a-z|A-Z].+\n)/gi," $1");
     document.getElementById("after").value = beforeText;
