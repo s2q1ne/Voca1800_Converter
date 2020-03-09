385ace21a02302bb44311283eab57d3da9892281
s2q1ne <amgkd9@naver.com>
2020-03-09 22:37:49 +0900

    \n filtering

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ var convert = ()=>{
     let beforeText = new String(document.getElementById("before").value);
     beforeText=beforeText.replace(/([0-9]+)\s([a-z|A-Z]+)(\s[a-z]{1,2}\.)/gi,"$2;");
     beforeText=beforeText.replace(/\s[a-z]{1,2}\./gi,",");
+    beforeText=beforeText.replace(/\n([^a-z|A-Z].+\n)/gi,"$1");
     document.getElementById("after").value = beforeText;
 }
