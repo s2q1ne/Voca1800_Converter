c9493bada7594a4ffd748a9235c31216e7cea0d7
s2q1ne <amgkd9@naver.com>
2020-03-09 22:49:22 +0900

    space reupload

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var convert = ()=>{
     let beforeText = new String(document.getElementById("before").value);
-    beforeText=beforeText.replace(/([0-9]+)\s([a-z|A-Z]+)(\s[a-z]{1,2}\.)/gi,"$2;");
-    beforeText=beforeText.replace(/\s[a-z]{1,2}\./gi,",");
-    beforeText=beforeText.replace(/\n([^a-z|A-Z].+\n)/gi," $1");
+    beforeText=beforeText
+    .replace(/([0-9]+)\s([a-z|A-Z]+)(\s[a-z]{1,2}\.)/gi,"$2;")
+    .replace(/\s[a-z]{1,2}\./gi,",")
+    .replace(/\n([^a-z|A-Z].+\n)/gi," $1");
     document.getElementById("after").value = beforeText;
 }
